2007c39b6f61877cf9517b54366fe1d9c954c6d3
robinissacdevelop <developerrobin18@gmail.com>
2025-07-19 17:02:12 +0530

    components made and fixed issue

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 // import logo from './logo.svg';
-import './App.css';
-import React from "react";
+
+import React from 'react';
 import { useEffect } from 'react';
-import form from './form.js';
+import Form from './components/form';
+
+
 
 function App() {
   useEffect(() => {
     document.body.style.backgroundColor = '#020024'; // Replace with your desired color code or name
   }, []);
   return (
-    <form />
-
+    <div className="App">
+      {/* <p class="italic text-red-600 font-semibold"> TODO LIST APP</p> */}
+      {/* class ="border-2 border-purple-500 flex justify-center" */}
+      <div class="pt-20 text-center">
+        <Form />
+      </div>
+    </div>
   );
 }
 
--- src/components/form.js
+++ src/components/form.js
@@ -4,10 +4,7 @@ import React from "react";
 
 function form(){
     return (
-         <div className="App">
-      {/* <p class="italic text-red-600 font-semibold"> TODO LIST APP</p> */}
-      {/* class ="border-2 border-purple-500 flex justify-center" */}
-      <div class="pt-20 text-center">
+        
         <form>
           <p class="text-xl italic font-serif text-pink-500">Hello Todoers..!</p>
           <div class="flex justify-center" className="form-group">
@@ -22,10 +19,9 @@ function form(){
           </div>
           <button type="submit" class="text-red-50 p-2 rounded-md borde-2 border-indigo-500 bg-indigo-500 hover:bg-green-600">Sign up</button>
         </form>
-      </div>
-
-    </div>
+     
     );
 }
-export default form();
+
+export default form;
    
